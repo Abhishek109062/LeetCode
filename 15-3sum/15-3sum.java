97d95c8e3f68da29b61622e41663d82dd13c929f
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-06-17 06:08:51 +0530

    Time: 16 ms (99.80%), Space: 57.7 MB (76.36%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,24 +1,97 @@
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        Set<List<Integer>> obj  = new HashSet<>();
-        for(int x=0;x<nums.length-2;x++)
-        {   
-            int y=x+1,z=nums.length-1;
-            while(y<z)
+class Solution 
+{
+    public List< List< Integer > > threeSum( int[] nums )
+    {
+        int maxVal = Integer.MIN_VALUE;
+        int minVal = Integer.MAX_VALUE;
+        int negNums = 0;
+        int posNums = 0;
+        List< List< Integer > > result = new LinkedList<>();
+        int zeroNums = 0;
+        for( int num : nums)
+        {
+            if( num < minVal )
             {
-                int sum=nums[x]+nums[y]+nums[z];
-                if(sum==0)
-                    obj.add(Arrays.asList(nums[x],nums[y++],nums[z--]));
-                else if(sum<0)
-                    y++;
-                else if(sum>0)
-                    z--;
+                minVal = num;   
             }
-            
+            if( num > maxVal )
+            {
+                maxVal = num;
+            }
+            if( num == 0 )
+            {
+                zeroNums = zeroNums + 1;   
+            }
+            else if( num > 0 )
+            {
+                posNums = posNums + 1;
+            }
+            else
+            {
+                negNums = negNums + 1;
+            }
         }
-        
-        return new ArrayList<>(obj);
-        
+        if( zeroNums >= 3 )
+        {
+            result.add( Arrays.asList( 0 , 0 , 0 ) );   
+        }
+        if( minVal >= 0 || maxVal <= 0 )
+        {
+            return result;    
+        }
+        int[] negNumMap = new int[ negNums ];
+        int[] posNumMap = new int[ posNums ];
+        int posStart = 0;
+        // if( maxVal + 2 * minVal > 0 )
+        // {
+        //     maxVal = -2 * minVal;    
+        // }
+        // else if( minVal + 2 * maxVal < 0 )
+        // {
+        //     minVal = -2 * maxVal;    
+        // }
+        byte[] numMap = new byte[ maxVal - minVal + 1 ];
+        negNums = 0;
+        posNums = 0;
+        //System.out.println( maxVal + " and " + minVal );
+        for( int num : nums )
+        {
+            if( num >= minVal && num <= maxVal )
+            {
+                if (numMap[num - minVal]++ != 0)
+                    numMap[num - minVal] = 2;
+                else
+                    if (num > 0) {
+                        posNumMap[posNums++] = num;
+                    } else if (num < 0) {
+                        negNumMap[negNums++] = num;
+                    }
+            }    
+        }
+        Arrays.parallelSort(posNumMap, 0, posNums);
+        Arrays.parallelSort(negNumMap, 0, negNums);
+        for (int i = negNums - 1; i >= 0; i--) {
+            int nv = negNumMap[i];
+            int minpv = (-nv) / 2;
+            while (posStart < posNums && posNumMap[posStart] < minpv)
+                posStart++;
+            for (int j = posStart; j < posNums; j++) {
+                int pv = posNumMap[j];
+                int ln = 0 - nv - pv;
+                if (ln >= nv && ln <= pv) {
+                    if (numMap[ln - minVal] == 0) {
+                        continue;
+                    } else if (ln == pv || ln == nv) {
+                        if (numMap[ln - minVal] > 1)
+                            result.add(Arrays.asList(nv, pv, ln));
+                    } else {
+                        result.add(Arrays.asList(nv, pv, ln));
+                    }
+                } else if (ln < nv) {
+                    break;
+                }
+            }
+        }
+        return result;
     }
 }
