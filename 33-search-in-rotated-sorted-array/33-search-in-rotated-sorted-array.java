ae9137a85013879feee3523802824037af198a9e
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-06-17 06:05:51 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (94.77%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,33 +1,18 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        
-        int find=-1;
-        if(nums.length==1)
-            return target==nums[0]?0:-1;
-        if(target<=nums[nums.length-1])
-        {
-            for(int x=nums.length-1;x>-1;x--)
-            {
-                if(nums[x]==target){
-                    find=x;
-                    break;
-                }
-                if(x-1>=0 && nums[x]<nums[x-1])
-                   break;
-            }
-            
-        }
-        else{
-            for(int x=0;x<nums.length-1;x++){
-                if(nums[x]==target){
-                    find=x;
-                    break;
-                }
-                if(nums[x]>nums[x+1])
-                    break;
-            }
-        }
-        
-        return find;
+  public int search(int[] nums, int target) {
+    int start = 0, end = nums.length - 1;
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      if (nums[mid] == target) return mid;
+      else if (nums[mid] >= nums[start]) {
+        if (target >= nums[start] && target < nums[mid]) end = mid - 1;
+        else start = mid + 1;
+      }
+      else {
+        if (target <= nums[end] && target > nums[mid]) start = mid + 1;
+        else end = mid - 1;
+      }
     }
+    return -1;
+  }
 }
