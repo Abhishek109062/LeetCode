5c2078a9b122c67655efe8a8ac1d4f50599002a5
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-06-21 13:00:25 +0530

    Time: 119 ms (86.59%), Space: 41.9 MB (68.13%) - LeetHub

---
+++ 79-word-search/79-word-search.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        char[] ch=word.toCharArray();
+        for(int x=0;x<board.length;x++){
+            for(int y=0;y<board[0].length;y++){
+               if(exist(board,ch,x,y,0))
+                   return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean exist(char[][] board,char[] ch,int x,int y,int i){
+        if(i==ch.length)
+            return true;
+        if(x<0 || y<0 || x==board.length || y==board[0].length)
+            return false;
+        if(board[x][y]!=ch[i])
+            return false;
+        
+        board[x][y]^=256;
+        boolean exist=exist(board,ch,x+1,y,i+1) || exist(board,ch,x-1,y,i+1) || exist(board,ch,x,y+1,i+1) || exist(board,ch,x,y-1,i+1);
+        board[x][y]^=256;
+        return exist;
+    }
+}
