1a1a5588710402bb7647cf804bdc309a63fe2fda
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-06-22 16:33:09 +0530

    Added solution - LeetHub

---
+++ Longest Arithmetic Progression - GFG/longest-arithmetic-progression.java
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            int ans = new Solution().lengthOfLongestAP(a, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int lengthOfLongestAP(int[] arr, int n) {
+        if(n==1){
+            return 1;
+        }
+        int res=0;
+        
+        HashMap<Integer,Integer>[] dp = new HashMap[n];
+        
+        for(int i=0; i<n; i++){
+            dp[i] = new HashMap<>();
+            for(int j=0; j<i; j++){
+                
+                int diff = arr[i] - arr[j];
+                
+                dp[i].put(diff, dp[j].getOrDefault(diff, 1)+1);
+                res = Math.max(res, dp[i].get(diff));
+            }
+        }
+        return res;
+    }
+}
