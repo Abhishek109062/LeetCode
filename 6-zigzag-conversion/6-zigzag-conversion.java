c717127e1b31c825809f93da2ab865b5a4299a15
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-07-03 10:36:31 +0530

    Time: 4 ms (95.74%), Space: 45.5 MB (69.69%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.java
+++ 6-zigzag-conversion/6-zigzag-conversion.java
@@ -1,19 +1,21 @@
 class Solution {
-    public String convert(String s, int nRows) {
-         char[] c = s.toCharArray();
-    int len = c.length;
-    StringBuffer[] sb = new StringBuffer[nRows];
-    for (int i = 0; i < sb.length; i++) sb[i] = new StringBuffer();
-    
-    int i = 0;
-    while (i < len) {
-        for (int idx = 0; idx < nRows && i < len; idx++) // vertically down
-            sb[idx].append(c[i++]);
-        for (int idx = nRows-2; idx >= 1 && i < len; idx--) // obliquely up
-            sb[idx].append(c[i++]);
-    }
-    for (int idx = 1; idx < sb.length; idx++)
-        sb[0].append(sb[idx]);
-    return sb[0].toString();
+    public String convert(String s, int numRows) {
+         if (numRows == 1 || numRows >= s.length()) {return s;}
+        
+        StringBuilder res = new StringBuilder();
+        int jump = 2*numRows - 2;
+        int length = s.length();
+        
+        // i is the index of each row
+        for (int i = 0; i < numRows; i++){
+            for (int j = 0; i + j < length; j += jump) {
+                res.append(s.charAt(i + j));
+                if (i != 0 && i != numRows - 1 && i + j + jump - 2*i < length) {
+                    res.append(s.charAt(i+j+jump-2*i));
+                }
+            }
+            
+        }
+        return res.toString();
     }
 }
