aeed0584564076da1f0c8b99975113569c220417
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-07-05 08:48:09 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (29.09%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -3,7 +3,6 @@ class Solution {
         
         for(int x=matrix.length-1;x>=0;x--){
             if(matrix[x][0]<=target){
-                System.out.println("hi");
                 for(int y=0;y<matrix[x].length;y++){
                     if(matrix[x][y]==target)
                         return true;
