6cbc05ba597fb4734794576609930a1bdef8c2ac
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-08-11 12:22:54 +0530

    Time: 2 ms (90.49%), Space: 44.8 MB (96.82%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -5,8 +5,8 @@ class Solution {
             for (int row = 0; row < height; row++) {
                 for (int col = 0; col < width; col++) {
                     if(row == 0 && col == 0) grid[row][col] = grid[row][col];
-                    else if(row == 0 && col != 0) grid[row][col] = grid[row][col] + grid[row][col - 1];
-                    else if(col == 0 && row != 0) grid[row][col] = grid[row][col] + grid[row - 1][col];
+                    else if(row == 0) grid[row][col] = grid[row][col] + grid[row][col - 1];
+                    else if(col == 0) grid[row][col] = grid[row][col] + grid[row - 1][col];
                     else grid[row][col] = grid[row][col] + Math.min(grid[row - 1][col], grid[row][col - 1]);
                 }
             }
