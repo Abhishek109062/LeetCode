6fa44df3bcaac8bc01b582909bc7e6a26b110409
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-08-28 06:48:28 -0700

    Time: 1 ms (100.00%), Space: 42.7 MB (23.83%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -1,19 +1,17 @@
 class Solution {
     public int longestPalindrome(String s) {
-        List<Character> temp=new ArrayList<>();
-        int count=0;
-        for(int x=0;x<s.length();x++){
-            if(temp.contains(s.charAt(x)))
-            {
-                temp.remove(Character.valueOf(s.charAt(x)));
-                count++;
-            }
-            else
-                temp.add(s.charAt(x));
+        int[] count = new int[128];
+        for (char c: s.toCharArray()){
+            count[c]++;
         }
-        if(!temp.isEmpty())
-            return 2*count+1;
-        else
-            return 2*count;
+
+        int ans = 0;
+        for (int value: count) {
+            ans += (value / 2) * 2;
+        }
+        //if all chars aren't even you can just add 1 at the end
+        //if they were all even ans and s.length would be the same
+        if( ans < s.length() ) ans++;
+        return ans;
     }
 }
