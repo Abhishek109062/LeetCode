4c7e4f4234e3e4dca94eb9cc925f681a4477f4d7
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-08-28 01:17:02 -0700

    Time: 10 ms (61.20%), Space: 47.9 MB (56.25%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -1,19 +1,14 @@
 class Solution {
     public int[][] diagonalSort(int[][] mat) {
-        int len=mat.length;
-        int len2=mat[0].length;
-        HashMap<Integer,PriorityQueue<Integer>> temp=new HashMap<>();
-        for(int x=0;x<len;x++){
-            for(int y=0;y<len2;y++){
-                //if(y==0)
-                    temp.putIfAbsent(x-y,new PriorityQueue<>());
-                temp.get(x-y).add(mat[x][y]);
-            }
-        }
-        for(int x=0;x<len;x++){
-            for(int y=0;y<len2;y++)
-                mat[x][y]=temp.get(x-y).poll();
-        }   
+        int m = mat.length, n = mat[0].length;
+        for (int r = m - 1, c = 0; r >= 0; r--) fillMatrix(mat, m, n, r, c);
+        for (int r = 0, c = 1; c < n - 1; c++) fillMatrix(mat, m, n, r, c);
         return mat;
+    }
+    private void fillMatrix(int[][] mat, int m, int n, int r, int c) {
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 0; r + i < m && c + i < n; i++) arr.add(mat[r + i][c + i]);
+        Collections.sort(arr);
+        for (int i = 0; r + i < m && c + i < n; i++) mat[r + i][c + i] = arr.get(i);
     }
 }
