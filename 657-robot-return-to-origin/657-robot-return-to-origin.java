cb61f9281c84e5af30913e687480cc3daab7fac8
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-08-29 10:01:34 -0700

    Time: 7 ms (74.45%), Space: 44.9 MB (50.54%) - LeetHub

--- 657-robot-return-to-origin/657-robot-return-to-origin.java
+++ 657-robot-return-to-origin/657-robot-return-to-origin.java
@@ -1,38 +1,17 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        List<Character> temp=new ArrayList<>();
+        int i=0,j=0;
         for(int x=0;x<moves.length();x++){
             char ch=moves.charAt(x);
-            if(ch=='R'){
-                if(temp.contains('L'))
-                    temp.remove(Character.valueOf('L'));
-                else
-                    temp.add('R');
-            }
-            else if(ch=='L')
-            {
-                    if(temp.contains('R'))
-                    temp.remove(Character.valueOf('R'));
-                else
-                    temp.add('L');
-                
-            }
-            else if(ch=='U'){
-                    if(temp.contains('D'))
-                    temp.remove(Character.valueOf('D'));
-                else
-                    temp.add('U');
-                
-            }
-            else if(ch=='D'){
-                    if(temp.contains('U'))
-                    temp.remove(Character.valueOf('U'));
-                else
-                    temp.add('D');
-                
-            }
-            }
-        return temp.isEmpty();
-        
+            if(ch=='R')
+                i++;
+            else if(ch=='U')
+                j++;
+            else if(ch=='D')
+                j--;
+            else
+                i--;
+        }
+        return (i==0 && j==0);
     }
 }
