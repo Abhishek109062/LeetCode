7d1e073cd9c9e55a4964f70f22624031b2c4ed1e
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-08-31 05:34:23 -0700

    Time: 7 ms (75.44%), Space: 55 MB (44.27%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -1,33 +1,39 @@
 class Solution {
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        int rows = heights.length, cols = heights[0].length;
-        boolean[][] pac = new boolean[rows][cols];
-        boolean[][] atl = new boolean[rows][cols];
+        int m = heights.length, n = heights[0].length;
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
         
-        for (int col = 0; col< cols; col++){
-            dfs(0, col, rows, cols, pac, heights[0][col], heights);
-            dfs(rows-1, col,rows, cols, atl, heights[rows-1][col], heights);
+        for (int y = 0; y < m; y++) {
+            dfs(heights, pacific, 0, y, 0);
+            dfs(heights, atlantic, n-1, y, 0);
         }
-        for (int row = 0; row<rows; row++){
-            dfs(row, 0,rows, cols, pac, heights[row][0], heights);
-            dfs(row, cols-1,rows, cols, atl, heights[row][cols-1], heights);
+        for (int x = 0; x < n; x++) {
+            dfs(heights, pacific, x, 0, 0);
+            dfs(heights, atlantic, x, m-1, 0);
         }
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        for (int i = 0; i < rows; i++)
-            for (int j = 0; j < cols; j++){
-                if (pac[i][j] && atl[i][j])
-                    result.add(Arrays.asList(i,j));
+        
+        List<List<Integer>> result = new ArrayList<>();
+        for (int y = 0; y < m; y++) {
+            for (int x = 0; x < n; x++) {
+                if (pacific[y][x] && atlantic[y][x]) {
+                    List<Integer> next = new ArrayList<>();
+                    next.add(y);
+                    next.add(x);
+                    result.add(next);
+                }
             }
+        }
         return result;
     }
-    private void dfs(int row, int col, int rows, int cols, boolean[][] visited, int prevHeight, int[][] heights){
-        if (row < 0 || row >= rows || col < 0 || col >= cols || visited[row][col] || prevHeight > heights[row][col])
-            return;
-        visited[row][col]= true;
-        dfs(row+1, col, rows, cols, visited, heights[row][col], heights);
-        dfs(row-1, col, rows, cols, visited, heights[row][col], heights);
-        dfs(row, col+1, rows, cols, visited, heights[row][col], heights);
-        dfs(row, col-1, rows, cols, visited, heights[row][col], heights);
-        
+    
+    private void dfs(int[][] heights, boolean[][] ocean, int x, int y, int prev) {
+        int m = heights.length, n = heights[0].length;
+        if (x < 0 || x >= n || y < 0 || y >= m || ocean[y][x] || heights[y][x] < prev) return;
+        ocean[y][x] = true;
+        dfs(heights, ocean, x+1, y, heights[y][x]);
+        dfs(heights, ocean, x, y+1, heights[y][x]);
+        dfs(heights, ocean, x-1, y, heights[y][x]);
+        dfs(heights, ocean, x, y-1, heights[y][x]);
     }
 }
