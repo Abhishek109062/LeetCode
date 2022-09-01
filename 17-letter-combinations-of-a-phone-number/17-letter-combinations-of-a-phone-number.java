a559dc25f660353af8b26af438b9de617f4e69dd
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-08-31 21:19:23 -0700

    Time: 5 ms (44.71%), Space: 40.8 MB (97.49%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,20 +1,20 @@
 class Solution {
+    List<String> ans=new ArrayList<>();
     public List<String> letterCombinations(String digits) {
         if(digits.length()==0)
             return new ArrayList<>();
         String[] phn={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        List<String> ans=new ArrayList<>();
-        comb(ans,digits,0,digits.length(),"",phn);
+        comb(digits,0,digits.length(),"",phn);
         return ans;
     }
-    public void comb(List<String> ans,String digits,int index,int len,String temp,String[] phn){
+    public void comb(String digits,int index,int len,String temp,String[] phn){
         if(len==index){
             ans.add(temp);
             return;
         }
         int cur=(int)(digits.charAt(index)-'0');
         for(int x=0;x<phn[cur].length();x++){
-            comb(ans,digits,index+1,len,temp+phn[cur].charAt(x),phn);
+            comb(digits,index+1,len,temp+phn[cur].charAt(x),phn);
         }
     }
 }
