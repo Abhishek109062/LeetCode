58aa76cafe6082934d1cc8f4ae754d5cd7ed95ae
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-09-02 04:37:35 -0700

    Time: 11 ms (96.34%), Space: 50.6 MB (16.63%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -1,25 +1,47 @@
 class Solution {
-    public int numDistinct(String S, String T) {
-    // array creation
-    int[][] mem = new int[T.length()+1][S.length()+1];
-
-    // filling the first row: with 1s
-    for(int j=0; j<=S.length(); j++) {
-        mem[0][j] = 1;
+    public int numDistinct(String s, String t) {
+        int[][] memo = new int[s.length() + 1][t.length() + 1];
+        for(int[] row: memo) {
+            Arrays.fill(row, -1);
+        }
+        return recursiveWithMemo(0, 0, s, t, memo);
+        //return iterativeWithTabulation(s, t);
     }
     
-    // the first column is 0 by default in every other rows but the first, which we need.
-    
-    for(int i=0; i<T.length(); i++) {
-        for(int j=0; j<S.length(); j++) {
-            if(T.charAt(i) == S.charAt(j)) {
-                mem[i+1][j+1] = mem[i][j] + mem[i+1][j];
-            } else {
-                mem[i+1][j+1] = mem[i+1][j];
+    private int iterativeWithTabulation(String s, String t) {
+        int M = s.length();
+        int N = t.length();
+        int[] tab = new int[N];
+        for (int i = M - 1 ; i >= 0; i--) {
+            int tij = 1;
+            for(int j = N - 1; j >= 0; j--) {
+                int oldj = tab[j];
+                if (s.charAt(i) == t.charAt(j)) {
+                    tab[j] += tij;
+                }
+                tij = oldj;
             }
         }
+        return tab[0];
     }
     
-    return mem[T.length()][S.length()];
-}
+    private int recursiveWithMemo(int i, int j, String s, String t, int[][]memo) {
+        int M = s.length();
+        int N = t.length();
+        
+        // Base case
+        if (i == M || j == N || M - i < N - j) {
+            return j == t.length() ? 1 : 0;
+        }
+        
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+        int ans = recursiveWithMemo(i + 1, j, s, t, memo);
+        if (s.charAt(i) == t.charAt(j)) {
+            ans += recursiveWithMemo(i + 1, j + 1, s, t, memo);
+        } 
+        memo[i][j] = ans;
+        return memo[i][j];
+    }
 }
