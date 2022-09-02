c665d20f981a8db6c05c162f5bcee4def11beeb9
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-09-02 07:19:29 -0700

    Time: 3 ms (90.86%), Space: 47.5 MB (80.62%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,29 +1,44 @@
 class Solution {
+    int count = 0;
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> temp=new ArrayList<>();
-        boolean[] flag=new boolean[nums.length];
-        per(nums,ans,temp,flag,nums.length);
-        return ans;
+        List<List<Integer>> list = new ArrayList<>();
+        int n = nums.length;
+        // count = 0;
+        permutations(0,nums,n-1,list);
+        // System.out.println(count);
+        return list;
     }
-    public void per(int[] nums,List<List<Integer>> ans, List<Integer> temp,boolean flag[],int len){
-        if(temp.size()==len){
-            if(ans.contains(temp))
-                return;
-            else
-            ans.add(new ArrayList<>(temp));
+    void permutations(int l,int nums[],int r,List<List<Integer>> list){
+        if(l==r){
+            List<Integer> temp = new ArrayList<>();
+            for(int e:nums) temp.add(e);
+            list.add(temp);
+            // count++;
             return;
-        }        
-        for(int x=0;x<len;x++){
-            if(!flag[x]){
-                flag[x]=true;
-                temp.add(nums[x]);
-                per(nums,ans,temp,flag,len);
-                temp.remove(temp.size()-1);
-                flag[x]=false;
-                
+        }
+        for(int i=l;i<=r;i++){
+            
+            // if(l!=i && nums[l]==nums[l-1]) continue;
+            // if(i!=l && nums[i]==nums[l]) continue;
+            boolean check = ifSwapable(nums,i, l);
+            if(check){
+                swap(nums,i,l);
+                permutations(l+1,nums,r,list);
+                swap(nums,i,l);
             }
         }
-        
     }
+    void swap(int nums[],int i,int l){
+        int t = nums[i];
+        nums[i] = nums[l];
+        nums[l] = t;
+    }
+    boolean ifSwapable(int nums[],int i,int l){
+        
+        for(int k = l;k<i;k++){
+            if(nums[k]==nums[i]) return false;
+        }
+        return true;
+    }
+
 }
