0f83659572baaeb44fb934daeeabcad3faac7924
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-09-02 19:32:52 -0700

    Time: 2 ms (99.94%), Space: 41 MB (96.79%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.java
+++ 43-multiply-strings/43-multiply-strings.java
@@ -1,21 +1,37 @@
 class Solution {
- public String multiply(String num1, String num2) {
-    int m = num1.length(), n = num2.length();
-    int[] pos = new int[m + n];
-   
-    for(int i = m - 1; i >= 0; i--) {
-        for(int j = n - 1; j >= 0; j--) {
-            int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0'); 
-            int p1 = i + j, p2 = i + j + 1;
-            int sum = mul + pos[p2];
-
-            pos[p1] += sum / 10;
-            pos[p2] = (sum) % 10;
+    public String multiply(String num1, String num2) {
+        if(num1.length() == 1 && num1.charAt(0) == '0')
+            return num1;
+        if(num2.length() == 1 && num2.charAt(0) == '0')
+            return num2;
+        
+        int[] res = new int[num1.length()+num2.length()];
+        for(int k1=0; k1<num1.length(); k1++){
+            int i = num1.length()-1-k1;
+            int d1 = num1.charAt(i)-'0';
+            if(d1 == 0) continue;
+            for(int k2=0; k2<num2.length(); k2++){
+                int j = num2.length()-1-k2;
+                int d2 = num2.charAt(j)-'0';
+                if(d2 == 0) continue;
+                int tmp = d1 * d2;
+                res[k1+k2] += tmp;   // 位数小的在前面
+            }
         }
-    }  
-    
-    StringBuilder sb = new StringBuilder();
-    for(int p : pos) if(!(sb.length() == 0 && p == 0)) sb.append(p);
-    return sb.length() == 0 ? "0" : sb.toString();
-}
+        for(int i=0; i<res.length; i++){    // 把每个位的和规整一下
+            if(res[i] >= 10) {
+                res[i+1] += res[i] / 10;    // assert res[i+1]下标一定有效
+                res[i] = res[i] % 10;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=res.length-1; i>=0; i--){
+            if(res[i] == 0 && i == res.length-1) {  // ignore first 0
+                continue;
+            }
+            char c = (char)(res[i]+'0');
+            sb.append(c);
+        }
+        return sb.toString();
+    }
 }
