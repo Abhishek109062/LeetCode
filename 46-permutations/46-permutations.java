4d151fcc31cd1c15333e3c2f0d7725cfa4070fad
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-09-03 04:14:17 -0700

    Time: 1 ms (98.39%), Space: 44.4 MB (75.09%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,26 +1,35 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> temp=new ArrayList<>();
-        boolean[] flag=new boolean[nums.length];
-        per(nums,ans,temp,flag,nums.length);
+        permute(nums,ans,0,nums.length);
         return ans;
     }
-    public void per(int[] nums,List<List<Integer>> ans, List<Integer> temp,boolean flag[],int len){
-        if(temp.size()==len){
-            ans.add(new ArrayList<>(temp));
+    public void permute(int[] nums,List<List<Integer>> ans,int i,int len){
+        if(i==len){
+            List<Integer> temp=new ArrayList<>();
+            for(int t:nums)
+                temp.add(t);
+            ans.add(temp);
             return;
-        }        
-        for(int x=0;x<len;x++){
-            if(!flag[x]){
-                flag[x]=true;
-                temp.add(nums[x]);
-                per(nums,ans,temp,flag,len);
-                temp.remove(temp.size()-1);
-                flag[x]=false;
-                
-            }
         }
         
+        for(int x=i;x<len;x++){
+            swap(nums,x,i);
+            permute(nums,ans,i+1,len);
+            swap(nums,x,i);
+        }
     }
+    public void swap(int nums[],int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    
+    // public boolean isSwappable(boolean flag[] ,int i ,int j){
+    //     if( !flag[j]){
+    //         return true;
+    //     }
+    //     return false;
+    // }
+    
 }
