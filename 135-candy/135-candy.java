28f156154bed3ab14bf5ddcbbe7cf12d5b65f8c4
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-09-09 04:18:04 -0700

    Time: 3 ms (89.77%), Space: 51.9 MB (45.16%) - LeetHub

---
+++ 135-candy/135-candy.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int candies[]  = new int[ratings.length];
+        for(int x = 0 ;x < candies.length ;x++)
+            candies[x]=1;//asssign 1 candy to every child
+        
+        for(int x = 1 ;x < ratings.length ;x++){
+            if(ratings[x] > ratings[x-1])
+                candies[x]= candies[x-1]+1;//right child as 1 more candy then left child
+        }
+        
+        for(int x = ratings.length-2 ;x >= 0 ;x--){
+            if(ratings[x] > ratings[x+1])
+                candies[x] = Math.max(candies[x],candies[x+1]+1);
+            
+        }
+        int sum = 0;
+        for( int x : candies)
+            sum += x;
+        
+        return sum;
+    }
+}
