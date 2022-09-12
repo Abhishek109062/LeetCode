08e8b46afb17df112ed083f2347f7161fd78a026
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-09-12 07:23:55 -0700

    Time: 13 ms (23.01%), Space: 46 MB (40.03%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,53 +1,46 @@
-class Solution {
+public class Solution {
     public List<List<String>> solveNQueens(int n) {
-        char [][]board=new char[n][n];
-        for(int i=0;i<n;i++)
-            for(int j=0;j<n;j++)
-                board[i][j]='.';
-        
-        List<List<String>> ans=new ArrayList<List<String>>();
-        int leftrow[]=new int[n];
-        int lowerdiagonal[]=new int[2*n-1];
-        int upperdiagonal[]=new int[2*n-1];
-        solver(0,board,ans,leftrow,lowerdiagonal,upperdiagonal);
-        return ans;
-    }    
-        
-        
-        
-     public void solver(int col, char[][] board,List<List<String>> ans,int[] leftrow,int[] lowerdiagonal,int [] upperdiagonal){
+        char[][] board = new char[n][n];
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                board[i][j] = '.';
+        List<List<String>> res = new ArrayList<List<String>>();
+        dfs(board, 0, res);
+        return res;
+    }
     
-         if(col==board.length){
-             ans.add(adder(board));
-             return;
-         }
-         
-    for(int row=0;row<board.length;row++ ){
-        if(leftrow[row]==0 && lowerdiagonal[row+col]==0 && upperdiagonal[board.length-1+row-col]==0){
-            board[row][col]='Q';
-            leftrow[row]=1;
-            lowerdiagonal[row+col]=1;
-            upperdiagonal[board.length-1+row-col]=1;
-            solver(col+1,board,ans,leftrow,lowerdiagonal,upperdiagonal);
-            board[row][col]='.';
-            leftrow[row]=0;
-            lowerdiagonal[row+col]=0;
-            upperdiagonal[board.length-1+row-col]=0;
+    private void dfs(char[][] board, int colIndex, List<List<String>> res) {
+        if(colIndex == board.length) {
+            res.add(construct(board));
+            return;
         }
-    }         
- }
-       
-public List<String>   adder(char [][]board){
-       /* List<String> ds=new ArrayList<String>();
-        for(int i=0;i<board.length;i++){
-            ds.add(board[i]);
+        
+        for(int i = 0; i < board.length; i++) {
+            if(validate(board, i, colIndex)) {
+                board[i][colIndex] = 'Q';
+                dfs(board, colIndex + 1, res);
+                board[i][colIndex] = '.';
+            }
         }
-        return ds;*/
-    List < String > res = new LinkedList < String > ();
-        for (int i = 0; i < board.length; i++) {
+    }
+    
+    private boolean validate(char[][] board, int x, int y) {
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < y; j++) {
+                if(board[i][j] == 'Q' && (x + j == y + i || x + y == i + j || x == i))
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private List<String> construct(char[][] board) {
+        List<String> res = new LinkedList<String>();
+        for(int i = 0; i < board.length; i++) {
             String s = new String(board[i]);
             res.add(s);
+        }
+        return res;
     }
-    return res;
-}
 }
