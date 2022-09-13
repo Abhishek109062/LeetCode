288e6f69d31961f44fdc7cee0e12c7044ab00fa0
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-09-12 19:23:05 -0700

    Time: 18 ms (18.91%), Space: 51.3 MB (21.24%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean validUtf8(int[] data) {
+        int utfbytes = 0;
+        for(int x : data){
+            String binary = Integer.toBinaryString(x);
+            
+            binary = binary.length() >= 8 ? binary.substring(binary.length() - 8) : "00000000".substring(binary.length()%8)+binary;
+            if(utfbytes == 0){
+                for(int y = 0; y < binary.length(); y++){
+                    if(binary.charAt(y) == '0')
+                        break;
+                    
+                    utfbytes++;
+                }
+                //System.out.println(utfbytes);
+                
+                if(utfbytes == 0)
+                    continue;
+                
+                if(utfbytes > 4 || utfbytes == 1)
+                    return false;
+            }
+            else{
+                if( !(binary.charAt(0) == '1' && binary.charAt(1) == '0'))
+                    return false;
+                }
+                
+                utfbytes--;
+         }
+        //System.out.println("hello");
+           return utfbytes == 0;
+        
+    }
+}
