0aec10ee4bc102711740865f6730d14cf6ebb796
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-09-14 21:03:14 -0700

    Time: 639 ms (6.14%), Space: 134.7 MB (53.89%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,25 +1,19 @@
 class Solution {
-    public int[] findOriginalArray(int[] changed) {
-        if(changed.length%2 == 1)
-            return new int[0];
-        Map<Integer,Integer> ans = new TreeMap<>();
-        for(int a:changed)
-            ans.put(a,ans.getOrDefault(a,0)+1);
-        
-        int ar[] = new int[changed.length/2];
-        int index = 0;
-        for(int a : ans.keySet()){
-            if(ans.get(a) > ans.getOrDefault(a*2,0))
-            {
-                //System.out.println("hello");
+    public int[] findOriginalArray(int[] A) {
+        int n = A.length, i = 0;
+        if (n % 2 == 1) return new int[0];
+        int[] res = new int[n / 2];
+        Map<Integer, Integer> count = new TreeMap<>();
+        for (int a : A)
+            count.put(a, count.getOrDefault(a, 0) + 1);
+        for (int x : count.keySet()) {
+            if (count.get(x) > count.getOrDefault(x + x, 0))
                 return new int[0];
-            }
-            
-            for(int x=1; x <= ans.get(a);x++){
-                ar[index++] = a;
-            ans.put(a*2,ans.get(a*2)-1);
+            for (int j = 0; j < count.get(x); ++j) {
+                res[i++] = x;
+                count.put(x + x, count.get(x + x) - 1);
             }
         }
-        return ar;
+        return res;
     }
 }
