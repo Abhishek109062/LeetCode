07042cb19b7a2a1aa6ef774e2b02a9f228088a21
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-09-20 07:32:19 -0700

    Time: 3 ms (96.07%), Space: 42.2 MB (55.87%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,20 +1,18 @@
 class Solution {
     public int countSubstrings(String s) {
         int count = 0;
+        char ch[] = s.toCharArray();
         for(int x = 0; x < s.length(); x++){
-            count += countpal(s,x,x);
-            count += countpal(s,x,x+1);
+            count += countpal(ch,x,x);
+            count += countpal(ch,x,x+1);
         }
         return count;
     }
     
-    public int countpal(String s,int left, int right){
+    public int countpal(char[] ch,int left, int right){
         int count = 0;
-        while(left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+        while(left >=0 && right < ch.length && ch[left--] == ch[right++])
             count++;
-            left--;
-            right++;
-        }
         return count;
     }
 }
