357e5c64e736d4406dc64badf4e347d2b222855c
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-09-20 21:01:59 -0700

    Time: 8 ms (39.23%), Space: 72.1 MB (49.84%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -1,24 +1,23 @@
 class Solution {
     public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
+        int[] res = new int[queries.length];
         int sum = 0;
-        for(int x = 0; x < nums.length; x++)
-            if(nums[x] % 2 == 0)
-                sum += nums[x];
-        
-        int[] ans = new int[queries.length];
-        for(int x = 0; x < queries.length; x++){
-            int index = queries[x][1];
-            if(nums[index] % 2 == 0)
-                sum -= nums[index];
-            
-            nums[index] += queries[x][0];
-            
-            if(nums[index] % 2 == 0)
-                sum += nums[index];
-            
-            ans[x] = sum;
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                sum += num;
+            }
         }
-        
-        return ans;
+        for (int i = 0; i < queries.length; i++) {
+            int idx = queries[i][1];
+            if (nums[idx] % 2 == 0) {
+                sum -= nums[idx];
+            }
+            nums[idx] += queries[i][0];
+            if (nums[idx] % 2 == 0) {
+                sum += nums[idx];
+            }
+            res[i] = sum;
+        }
+        return res;
     }
 }
