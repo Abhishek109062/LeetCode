b1c9e5068c829351e57e505cc35b75321e27bf8f
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-09-21 11:54:45 -0700

    Time: 4 ms (75.22%), Space: 42.5 MB (80.12%) - LeetHub

--- 223-rectangle-area/223-rectangle-area.java
+++ 223-rectangle-area/223-rectangle-area.java
@@ -1,7 +1,26 @@
 class Solution {
-    int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-    int left = Math.max(A,E), right = Math.max(Math.min(C,G), left);
-    int bottom = Math.max(B,F), top = Math.max(Math.min(D,H), bottom);
-    return (C-A)*(D-B) - (right-left)*(top-bottom) + (G-E)*(H-F);
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        // Calculate first rectangle square
+        int firstRectSq = (ax2 - ax1) * (ay2 - ay1);
+
+        // Calculate second rectangle square
+        int secondRectSq = (bx2 - bx1) * (by2 - by1);
+
+        // Calculate intersection rectangle square
+        int ix1 = 0, ix2 = 0;
+        if (bx1 < ax2 && bx2 > ax1) {
+            // There is intersection by x
+            ix1 = Math.max(ax1, bx1);
+            ix2 = Math.min(ax2, bx2);
+        }
+        int iy1 = 0, iy2 = 0;
+        if (by1 < ay2 && by2 > ay1) {
+            // There is intersection by y
+            iy1 = Math.max(ay1, by1);
+            iy2 = Math.min(ay2, by2);
+        }
+        int intersectionRectSq = (ix2 - ix1) * (iy2 - iy1);
+
+        return firstRectSq + secondRectSq - intersectionRectSq;    
     }
 }
