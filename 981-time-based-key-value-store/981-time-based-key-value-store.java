a938540001351cd9c9c9c1e8ba7bfbe630789b8d
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-10-06 21:48:27 +0530

    Time: 400 ms (31.12%), Space: 216.9 MB (62.33%) - LeetHub

---
+++ 981-time-based-key-value-store/981-time-based-key-value-store.java
@@ -0,0 +1,28 @@
+class TimeMap {
+    HashMap<String,HashMap<Integer,String>> hm = new HashMap<>();
+    HashMap<String,Integer> lastAdded= new HashMap<>();
+    
+    public void set(String key, String value, int timestamp) {
+        if(!hm.containsKey(key))
+        {
+            HashMap<Integer,String> temp=new HashMap<>();
+            hm.put(key,temp);
+            HashMap<String,Integer> temp1=new HashMap<>();
+            lastAdded.put(key,timestamp);
+        }
+        hm.get(key).put(timestamp,value);
+        if(timestamp>lastAdded.get(key)) lastAdded.put(key,timestamp);
+    }
+    
+    public String get(String key, int timestamp) {
+        if(!hm.containsKey(key)) return "";
+        if(timestamp>=lastAdded.get(key)) return hm.get(key).get(lastAdded.get(key));
+        int found=-1;
+        for(Map.Entry<Integer,String> i:hm.get(key).entrySet())
+        {
+            if(i.getKey()>found && i.getKey()<=timestamp) found=i.getKey();
+        }
+        return found==-1?"":hm.get(key).get(found);
+    }
+    
+}
