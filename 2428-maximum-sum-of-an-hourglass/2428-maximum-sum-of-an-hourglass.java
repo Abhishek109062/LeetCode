7db92022c1d3fa21858502a13f9cfb4660652b5d
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-10-10 12:15:50 +0530

    Time: 2 ms (100.00%), Space: 44.2 MB (86.14%) - LeetHub

--- 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.java
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.java
@@ -1,18 +1,29 @@
 class Solution {
+    private int hourglassSum(int[][] grid, int row, int col) {
+        int result = 0;
+
+        result += grid[row-1][col-1];
+        result += grid[row-1][col];
+        result += grid[row-1][col+1];
+        result += grid[row][col];
+        result += grid[row+1][col-1];
+        result += grid[row+1][col];
+        result += grid[row+1][col+1];
+
+        return result;
+    }
+
     public int maxSum(int[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        if(row < 3 || col < 3)
-            return 0;
-        
-        int sum = 0;
-        for(int x = 0; x < row-2; x++){
-            for(int y = 0;y < col-2; y++){
-                int count = grid[x][y] + grid[x][y+1] + grid[x][y+2] + grid[x+1][y+1] + grid[x+2][y] + grid[x+2][y+1] + grid[x+2][y+2];
-                sum = Math.max(count, sum);
+        int result = -1;
+        for (int i = 1; i < grid.length - 1; i++) {
+            for (int j = 1; j < grid[0].length - 1; j++) {
+                int sum = hourglassSum(grid, i, j);
+                if (result < sum) {
+                    result = sum;
+                }
             }
         }
-        
-        return sum;
+
+        return result;
     }
 }
