b2c9f539641d7b48b006f4de5e8a9aa0437d7169
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-10-10 12:19:02 +0530

    Time: 5 ms (88.15%), Space: 44.2 MB (88.39%) - LeetHub

--- 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.java
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.java
@@ -2,9 +2,6 @@ class Solution {
     public int maxSum(int[][] grid) {
         int row = grid.length;
         int col = grid[0].length;
-        if(row < 3 || col < 3)
-            return 0;
-        
         int sum = 0;
         for(int x = 1; x < row-1; x++){
             for(int y = 1;y < col-1; y++){
