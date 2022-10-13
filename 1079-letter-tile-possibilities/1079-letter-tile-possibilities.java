13445768c48b75f10bdf02847f0a8d5041a75245
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-10-13 15:40:20 +0530

    Time: 3 ms (96.52%), Space: 40.2 MB (96.26%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -1,19 +1,43 @@
 class Solution {
-    public int numTilePossibilities(String tiles) {
-        Set<String> ans = new HashSet<>();
-        boolean[] visit = new boolean[tiles.length()];
-        diff(tiles, "", ans, visit);
-        return ans.size()-1;
-    }
-    public void diff(String s, String temp,Set<String> ans, boolean[] visit){
-        ans.add(temp);
+//     public int numTilePossibilities(String tiles) {
+//         Set<String> ans = new HashSet<>();
+//         boolean[] visit = new boolean[tiles.length()];
+//         diff(tiles, "", ans, visit);
+//         return ans.size()-1;
+//     }
+//     public void diff(String s, String temp,Set<String> ans, boolean[] visit){
+//         ans.add(temp);
         
-        for(int x = 0; x < s.length(); x++){
-            if(!visit[x]){
-                visit[x] = true;
-                diff(s, temp+s.charAt(x), ans, visit);
-                visit[x] = false;
-            }
-        }
+//         for(int x = 0; x < s.length(); x++){
+//             if(!visit[x]){
+//                 visit[x] = true;
+//                 diff(s, temp+s.charAt(x), ans, visit);
+//                 visit[x] = false;
+//             }
+//         }
+//     }
+    
+    //Second Method using freq array
+    public int numTilePossibilities(String tiles) {
+        int freq[] = new int[26];
+        // List<String> ans = new ArrayList<>();
+        for(int x = 0; x < tiles.length(); x++)
+            freq[tiles.charAt(x) - 'A']++;
+        
+        return tilePossible(freq);
     }
+    
+    public static int tilePossible(int[] freq){
+        int count = 0;
+        for(int x = 0; x < freq.length; x++){
+            if(freq[x] >= 1){
+                // char ch = (char)(x + 'A');
+                freq[x]--;
+                count += tilePossible(freq) + 1;
+                freq[x]++;
+            } 
+        }
+        return count;
+    }
+    
 }
