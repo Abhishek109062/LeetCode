df36d5ced75c4a7b86c242505b9292c66547b923
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-10-13 15:18:21 +0530

    Time: 121 ms (14.19%), Space: 54.4 MB (29.29%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numTilePossibilities(String tiles) {
+        Set<String> ans = new HashSet<>();
+        boolean[] visit = new boolean[tiles.length()];
+        diff(tiles, "", ans, visit);
+        return ans.size()-1;
+    }
+    public void diff(String s, String temp,Set<String> ans, boolean[] visit){
+        ans.add(temp);
+        
+        for(int x = 0; x < s.length(); x++){
+            if(!visit[x]){
+                visit[x] = true;
+                diff(s, temp+s.charAt(x), ans, visit);
+                visit[x] = false;
+            }
+        }
+    }
+}
