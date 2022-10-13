17ce902f60937b54a5d0ea6f2f25c54834db03ba
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-10-13 09:51:34 +0530

    Time: 11 ms (61.88%), Space: 47 MB (40.84%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.java
+++ 0539-minimum-time-difference/0539-minimum-time-difference.java
@@ -8,13 +8,12 @@ class Solution {
             time[i++] = Integer.parseInt(s.substring(0,2))*60 + Integer.parseInt(s.substring(3));
         
         Arrays.sort(time);
-        int min = Integer.MAX_VALUE;
+        int min = max + time[0] - time[len-1];
         for(int x = 0; x < len-1; x++){
             int temp = Math.min((time[x+1] - time[x]), max+time[x]-time[x+1]);
             min = Math.min(min, temp);
         }
         
-        min = Math.min(max + time[0] - time[len-1],min);
         return min;
     }
 }
