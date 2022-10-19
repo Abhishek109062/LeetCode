cfd0f8bc3bea1d2cd8eea4b1519f14681d99b061
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-10-19 14:09:31 +0530

    Time: 122 ms (88.54%), Space: 40 MB (98.13%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        
+        int m = board.length;
+        int n = board[0].length;
+        
+        for(int r = 0; r < m; r++)
+            for(int c = 0; c < n; c++)
+                if(backtrack(board, m, n, r, c, word, 0))
+                    return true;
+        
+        return false;
+    }
+    
+    public boolean backtrack(char[][] board, int m, int n, int r, int c, String word, int charIndx){
+        
+        if(word.length() == charIndx)
+            return true;
+        
+        if(r < 0 || r >= m || c < 0 || c >= n)
+            return false;
+        
+        if(board[r][c] != word.charAt(charIndx))
+            return false;
+        
+        board[r][c] = '@'; 
+        
+        if(backtrack(board, m, n, r, c+1, word, charIndx+1)
+          || backtrack(board, m, n, r, c-1, word, charIndx+1)
+          || backtrack(board, m, n, r+1, c, word, charIndx+1)
+          || backtrack(board, m, n, r-1, c, word, charIndx+1))
+            return true;
+        
+        board[r][c] = word.charAt(charIndx); 
+        return false;
+    }
+}
