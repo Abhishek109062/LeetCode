abd44bb633d66cbf8ae9b5cad5a33bcf81fe1946
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-10-28 19:24:12 +0530

    Time: 4 ms (97.16%), Space: 44.3 MB (50.99%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -14,12 +14,32 @@ private int eval(String s, int[] p){
     while(i < s.length()){
         char c = s.charAt(i);
         switch(c){
-            case '+': val = val + oper * num; num = 0; oper = 1; i++; break;// end of number and             set operator
-            case '-': val = val + oper * num; num = 0; oper = -1; i++; break;// end of number and set operator
-            case '(': p[0] = i + 1; val = val + oper * eval(s, p); i = p[0]; break; // start a new eval
-            case ')': p[0] = i + 1; return val + oper * num; // end current eval and return. Note that we need to deal with the last num
-            case ' ': i++; continue;
-            default : num = num * 10 + c - '0'; i++;
+            case '+': 
+                val = val + oper * num; 
+                num = 0; 
+                oper = 1; 
+                i++; 
+                break;
+            case '-': 
+                val = val + oper * num; 
+                num = 0; 
+                oper = -1; 
+                i++; 
+                break;
+            case '(': 
+                p[0] = i + 1; 
+                val = val + oper * eval(s, p); 
+                i = p[0]; 
+                break; 
+            case ')': 
+                p[0] = i + 1; 
+                return val + oper * num; 
+            case ' ': 
+                i++; 
+                continue;
+            default : 
+                num = num * 10 + c - '0'; 
+                i++;
         }
     }
     return val;
