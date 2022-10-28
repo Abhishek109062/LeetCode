88849f4d41e3810f8308133ff42363969eedf258
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-10-28 18:42:08 +0530

    Time: 19 ms (74.02%), Space: 44.8 MB (49.51%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> temp = new Stack<>();
+        char sign = '+';
+        int num = 0;
+        
+        for(int x = 0; x < s.length(); x++){
+            char ch = s.charAt(x);
+            if(ch >= '0' && ch <= '9')
+                num = num * 10 + (ch - '0');
+            
+            if(!(ch >= '0' && ch <= '9') && ch != ' ' || x == s.length() - 1){
+                if(sign == '+')
+                    temp.push(num);
+                
+                if(sign == '-')
+                    temp.push(-1*num);
+                
+                if(sign == '*')
+                    temp.push(temp.pop() * num);
+                
+                if(sign == '/')
+                    temp.push(temp.pop() / num);
+                
+                sign = ch;
+                num = 0;
+            }
+        }
+        
+        int ans = 0;
+        for(int x : temp)
+            ans += x;
+        
+        return ans;
+    }
+}
