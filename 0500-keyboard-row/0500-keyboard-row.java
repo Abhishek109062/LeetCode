5ec06196e1da4aff2ae0b60df5b7364e10fe095a
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-10-28 12:18:34 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (64.59%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.java
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -1,5 +1,26 @@
 class Solution {
     public String[] findWords(String[] words) {
-    return Stream.of(words).filter(s -> s.toLowerCase().matches("[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*")).toArray(String[]::new);
-}
+        int m1 = getMask("qwertyuiop");
+        int m2 = getMask("asdfghjkl");
+        int m3 = getMask("zxcvbnm");
+        List<String> array = new ArrayList<>();
+        for(String word: words){
+            int t = getMask(word);
+            if((t&m1) == t || (t&m2) == t || (t&m3)==t)
+                array.add(word);
+        }
+        String[] res = new String[array.size()];
+        for(int i = 0; i < res.length; i++){
+            res[i] = array.get(i);
+        }
+        return res;
+    }
+    
+    public int getMask(String str){
+        int m = 0;
+        for(char c: str.toCharArray()){
+            m = m | (1 << (c - 97));
+        }
+        return m;
+    }
 }
