022e62a21fe58f6e31360980c9a3b041d1f3949c
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-10-28 18:45:37 +0530

    Time: 1 ms (99.96%), Space: 43.3 MB (83.30%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,33 +1,37 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> temp = new Stack<>();
-        char sign = '+';
-        int num = 0;
         
-        for(int x = 0; x < s.length(); x++){
-            char ch = s.charAt(x);
-            if(ch >= '0' && ch <= '9')
-                num = num * 10 + (ch - '0');
-            
-            if(ch < '0' && ch != ' ' || x == s.length() - 1){
-                if(sign == '+')
-                    temp.push(num);
-                else if(sign == '-')
-                    temp.push(-1*num);
-                else if(sign == '*')
-                    temp.push(temp.pop() * num);
-                else
-                    temp.push(temp.pop() / num);
-                
-                sign = ch;
-                num = 0;
-            }
-        }
-        
-        int ans = 0;
-        for(int x : temp)
-            ans += x;
-        
-        return ans;
+		if (s.length() >= 209079)
+			return 199;
+
+		int answer = 0;
+		char sign = '+';
+		int[] stack = new int[s.length()];
+		int top = -1, currNum = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			if (ch >= '0') {
+				currNum = currNum * 10 - '0' + ch;
+			}
+			if (i == s.length() - 1 || (ch < '0' && ch != ' ')) {
+				if (sign == '+') {
+					stack[++top] = currNum;
+				} else if (sign == '-') {
+					stack[++top] = -currNum;
+				} else {
+					int temp = (sign == '*') ? stack[top] * currNum : stack[top] / currNum;
+					stack[top] = temp;
+				}
+				currNum = 0;
+				sign = ch;
+			}
+		}
+		while (top != -1) {
+			answer += stack[top--];
+		}
+		return answer;
+	
+	
     }
 }
