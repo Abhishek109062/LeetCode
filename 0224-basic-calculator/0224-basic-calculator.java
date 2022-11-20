8bf20b93cd3acbee8489224fbef20d0461193edd
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-11-20 08:02:43 +0530

    Time: 4 ms (97.41%), Space: 44.1 MB (68.12%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -6,43 +6,42 @@ class Solution {
     return eval(s, p);
 }
 // calculate value between parentheses
-private int eval(String s, int[] p){
-    int val = 0;
-    int i = p[0]; 
-    int oper = 1; //1:+ -1:-
-    int num = 0;
-    while(i < s.length()){
-        char c = s.charAt(i);
-        switch(c){
-            case '+': 
-                val = val + oper * num; 
-                num = 0; 
-                oper = 1; 
-                i++; 
-                break;
-            case '-': 
-                val = val + oper * num; 
-                num = 0; 
-                oper = -1; 
-                i++; 
-                break;
-            case '(': 
-                p[0] = i + 1; 
-                val = val + oper * eval(s, p); 
-                i = p[0]; 
-                break; 
-            case ')': 
-                p[0] = i + 1; 
-                return val + oper * num; 
-            case ' ': 
-                i++; 
-                continue;
-            default : 
-                num = num * 10 + c - '0'; 
-                i++;
+    public int eval(String s, int[] p){
+        int val = 0;
+        int i = p[0]; 
+        int oper = 1; //1:+ -1:-
+        int num = 0;
+        while(i < s.length()){
+            char c = s.charAt(i);
+            switch(c){
+                case '+': 
+                    val = val + oper * num; 
+                    num = 0; 
+                    oper = 1; 
+                    i++; 
+                    break;
+                case '-': 
+                    val = val + oper * num; 
+                    num = 0; 
+                    oper = -1; 
+                    i++; 
+                    break;
+                case '(': 
+                    p[0] = i + 1; 
+                    val = val + oper * eval(s, p); 
+                    i = p[0]; 
+                    break; 
+                case ')': 
+                    p[0] = i + 1; 
+                    return val + oper * num; 
+                case ' ': 
+                    i++; 
+                    continue;
+                default : 
+                    num = num * 10 + c - '0'; 
+                    i++;
+            }
         }
+        return val;
     }
-    return val;
-}
-
 }
