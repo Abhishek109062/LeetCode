3211eb26d234a9fb3d991339432cae6603d98f3b
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2022-12-04 08:30:39 +0530

    Time: 29 ms (30.79%), Space: 72.1 MB (80.57%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.java
+++ 2256-minimum-average-difference/2256-minimum-average-difference.java
@@ -1,33 +1,23 @@
 class Solution {
     public int minimumAverageDifference(int[] nums) {
+        long total = 0;
+        for(int num : nums)
+            total += num;
         int n = nums.length;
-        long left[] = new long[n];
-        long right[] = new long[n];
+        int min = 0;
+        int min_avg = 100000;
         long sum = 0;
-        for(int x = 0; x < n; x++){
-            sum += nums[x];
-            left[x] = sum;
-        }
-        
-        for(int x = n-1; x >= 0; x--){
-            right[x] = sum - left[x];
-        }
-        
-        int res = 0;
-        long min = Integer.MAX_VALUE;
-        
-        // for(int x = 0; x < n; x++)
-        //     System.out.println(left[x] + " " + right[x]);
-        
-        for(int x = 0; x < n; x++){
-            long diff = Math.abs(left[x]/(x+1) - (x == n - 1 ? 0 : right[x]/( n - x - 1)));
-            
-            if(diff < min){
-                min = diff;
-                res = x;
+        for(int i = 0; i < n-1; ++i) {
+            sum += nums[i];
+            //System.out.println("first = " + sum/(i+1) + ", second = " + (total-sum))
+            int avg = (int) Math.abs((sum / (i+1)) - ((total-sum)/(n-i-1)));
+            if(avg < min_avg) {
+                min_avg = avg;
+                min = i;
             }
         }
-        
-        return res;
+        if(total/n  < min_avg) //compute for last elem here to avoid divide by zero in for loop
+            min = n-1;
+        return min;
     }
 }
