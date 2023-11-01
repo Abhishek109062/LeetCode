5f935bfabddf00f0f5ab7682233cfe7bd71f823c
Abhishek Kumar <72486443+Abhishek109062@users.noreply.github.com>
2023-11-01 19:45:03 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (29.16%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -3,21 +3,20 @@ class Solution {
         int start = 0;
         int end = nums.length - 1;
         
-        for(int x = 0; x < nums.length; x++){
+        for(int x = 0; x <= end; x++){
             if(nums[x] == 0){
                 int temp = nums[x];
                 nums[x] = nums[start];
                 nums[start] = 0;
                 start++;
             }
-        }
-        
-        for(int x = nums.length - 1; x >= 0; x--){
+            
             if(nums[x] == 2){
                 int temp = nums[x];
                 nums[x] = nums[end];
                 nums[end] = 2;
                 end--;
+                x--;
             }
         }
     }
